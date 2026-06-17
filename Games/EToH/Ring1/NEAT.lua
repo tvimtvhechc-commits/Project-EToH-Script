62197e50529311ec78bb0e460b6169fba7182d69
tvimtvhechc-commits <tvimtvhechc@gmail.com>
2026-06-17 20:13:22 +0200

    Add files via upload

---
+++ Games/EToH/Ring1/NEAT.lua
@@ -0,0 +1,24 @@
+return function()
+    return {
+        "jump",
+        workspace.Towers.NEAT.Obby.Part,
+        workspace.Towers.NEAT.Obby:GetChildren()[2],
+        workspace.Towers.NEAT.Obby:GetChildren()[3],
+        workspace.Towers.NEAT.Obby:GetChildren()[4],
+        workspace.Towers.NEAT.Obby:GetChildren()[5],
+        workspace.Towers.NEAT.Obby:GetChildren()[27],
+        workspace.Towers.NEAT.Obby:GetChildren()[8],
+        workspace.Towers.NEAT.Obby:GetChildren()[10],
+        workspace.Towers.NEAT.Obby:GetChildren()[28],
+        workspace.Towers.NEAT.Obby:GetChildren()[16],
+        workspace.Towers.NEAT.Obby:GetChildren()[18],
+        workspace.Towers.NEAT.Frame:GetChildren()[11],
+        workspace.Towers.NEAT.Frame:GetChildren()[27],
+        workspace.Towers.NEAT.Frame:GetChildren()[22],
+        workspace.Towers.NEAT.Obby:GetChildren()[46],
+        workspace.Towers.NEAT.Obby:GetChildren()[62],
+        workspace.Towers.NEAT.WinPad
+ 
+
+    }
+end
