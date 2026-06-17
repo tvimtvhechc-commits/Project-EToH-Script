fd93ab0621d00285f6681bb0fde2a4758862d069
tvimtvhechc-commits <tvimtvhechc@gmail.com>
2026-06-17 20:10:05 +0200

    Add files via upload

---
+++ Games/EToH/Ring1/R1TR.lua
@@ -0,0 +1,17 @@
+return function()
+    return {
+        "ToAST",
+        "ToA",
+        "ToM",
+        "ToNI",
+        "ToH",
+        "ToK",
+        "ToKY",
+        "ToS",
+        "ToSP",
+        "ToR",
+        "ToIE",
+        "ToTS",
+        "CoLS"
+    }
+end
